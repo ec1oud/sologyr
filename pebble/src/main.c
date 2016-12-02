3b59e3af9303db31a4831f7413a385188d375733
Shawn Rutledge <s@ecloud.org>
2016-12-02 08:26:53 +0100

    draw pie wedge for daylight

--- pebble/src/main.c
+++ pebble/src/main.c
@@ -6,11 +6,22 @@ static BitmapLayer *bluetooth_layer;
 static BitmapLayer *charging_layer;
 static Layer *batteryGraphLayer;
 static int batteryPct = 0;
-static bool bluetoothConnected = 0;
 static int fontSize = 0;
-//~ static Layer *circleLayer;
+static uint8_t sunriseHour = 8;
+static uint8_t sunriseMinute = 50;
+static uint8_t sunsetHour = 15;
+static uint8_t sunsetMinute = 24;
+static bool bluetoothConnected = 0;
+static Layer *circleLayer;
 static GBitmap *bluetooth_bitmap = NULL;
 static GBitmap *charging_bitmap = NULL;
+
+static const int minutesPerDay = 24 * 60;
+
+static int min(int one, int other)
+{
+	return (one < other ? one : other);
+}
 
 static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed)
 {
@@ -21,35 +32,50 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed)
 	text_layer_set_text(text_time_layer, time_text);
 }
 
+static void updateCircleLayer(Layer *layer, GContext* ctx)
+{
+	GRect layerBounds = layer_get_bounds(layer);
+	GRect fillCircle = grect_crop(layerBounds, 2);
+	graphics_context_set_stroke_color(ctx, GColorWhite);
+	graphics_draw_circle(ctx, grect_center_point(&layerBounds), layerBounds.size.w / 2);
+	int sunriseAngle = (sunriseHour * 60 + sunriseMinute) * 360 / minutesPerDay - 180;
+	int sunsetAngle = (sunsetHour * 60 + sunsetMinute) * 360 / minutesPerDay - 180;
+	//~ graphics_context_set_fill_color(ctx, GColorRed);
+	//~ graphics_fill_radial(ctx, fillCircle, GCornerNone, fillCircle.size.w / 2,  DEG_TO_TRIGANGLE(sunsetAngle - 2),  DEG_TO_TRIGANGLE(sunsetAngle));
+	graphics_context_set_fill_color(ctx, GColorYellow);
+	graphics_fill_radial(ctx, fillCircle, GCornerNone, fillCircle.size.w / 2,  DEG_TO_TRIGANGLE(sunriseAngle),  DEG_TO_TRIGANGLE(sunsetAngle));
+	//~ graphics_fill_radial(ctx, fillCircle, GCornerNone, fillCircle.size.w / 2,  DEG_TO_TRIGANGLE(sunriseAngle - 2),  DEG_TO_TRIGANGLE(sunriseAngle));
+}
+
 static void updateBatteryGraphLayer(Layer *layer, GContext* ctx)
 {
-	GRect battBounds = layer_get_bounds(layer);
-	//~ APP_LOG(APP_LOG_LEVEL_DEBUG, "updateBatteryGraphLayer bounds %d %d %d %d", battBounds.origin.x, battBounds.origin.y, battBounds.size.w, battBounds.size.h);
+	GRect layerBounds = layer_get_bounds(layer);
+	//~ APP_LOG(APP_LOG_LEVEL_DEBUG, "updateBatteryGraphLayer bounds %d %d %d %d", layerBounds.origin.x, layerBounds.origin.y, layerBounds.size.w, layerBounds.size.h);
 	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_rect(ctx, battBounds, 0, GCornerNone);
+	graphics_fill_rect(ctx, layerBounds, 0, GCornerNone);
 	graphics_context_set_fill_color(ctx, GColorWhite);
 	graphics_context_set_stroke_color(ctx, GColorWhite);
 	{
 		GRect knob;
 		int knobWidth = 2;
-		int knobHeight = battBounds.size.h / 2;
+		int knobHeight = layerBounds.size.h / 2;
 		knobHeight -= knobHeight % 2;
-		knob.origin.x = battBounds.size.w - knobWidth;
-		knob.origin.y = battBounds.size.h / 2 - knobHeight / 2;
+		knob.origin.x = layerBounds.size.w - knobWidth;
+		knob.origin.y = layerBounds.size.h / 2 - knobHeight / 2;
 		knob.size.w = knobWidth;
 		knob.size.h = knobHeight;
 		graphics_fill_rect(ctx, knob, 1, GCornerTopRight | GCornerBottomRight);
-		battBounds.size.w -= knobWidth;
+		layerBounds.size.w -= knobWidth;
 	}
 	{
 		GRect fill;
 		fill.origin.x = 2;
 		fill.origin.y = 2;
-		fill.size.w = batteryPct * (battBounds.size.w - 4) / 100;
-		fill.size.h = battBounds.size.h - 4;
+		fill.size.w = batteryPct * (layerBounds.size.w - 4) / 100;
+		fill.size.h = layerBounds.size.h - 4;
 		graphics_fill_rect(ctx, fill, 0, GCornerNone);
 	}
-	graphics_draw_rect(ctx, battBounds);
+	graphics_draw_rect(ctx, layerBounds);
 }
 
 static void handle_tap(AccelAxisType axis, int32_t direction)
@@ -72,7 +98,7 @@ static void handle_battery(BatteryChargeState charge_state)
 
 static void window_window_load(Window *window) {
 	Layer *window_layer = window_get_root_layer(window);
-	//~ GRect bounds = layer_get_bounds(window_layer);
+	GRect bounds = layer_get_bounds(window_layer);
 
 	text_time_layer = text_layer_create(GRect(3, -10, 72, 28));
 	text_layer_set_text_color(text_time_layer, GColorWhite);
@@ -96,11 +122,11 @@ static void window_window_load(Window *window) {
 	layer_add_child(window_layer, bitmap_layer_get_layer(charging_layer));
 	charging_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CHARGING);
 
-	//~ circleLayer = layer_create(GRect(0, 16, bounds.size.w, bounds.size.h - 16));
-	//~ layer_set_update_proc(circleLayer, updatecircleLayer);
-	//~ layer_add_child(window_layer, circleLayer);
-
-	//~ layer_mark_dirty(circleLayer);
+	int diameter = min(bounds.size.w - 1, bounds.size.h - 16 - 1);
+	circleLayer = layer_create(GRect(0, 16, diameter, diameter));
+	layer_set_update_proc(circleLayer, updateCircleLayer);
+	layer_add_child(window_layer, circleLayer);
+	layer_mark_dirty(circleLayer);
 
 	tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
 	bluetooth_connection_service_subscribe(&handle_bluetooth);
@@ -111,7 +137,7 @@ static void window_window_load(Window *window) {
 
 static void window_window_unload(Window *window) {
 	tick_timer_service_unsubscribe();
-	//~ layer_destroy(circleLayer);
+	layer_destroy(circleLayer);
 	text_layer_destroy(text_time_layer);
 	bitmap_layer_destroy(bluetooth_layer);
 	gbitmap_destroy(bluetooth_bitmap);
