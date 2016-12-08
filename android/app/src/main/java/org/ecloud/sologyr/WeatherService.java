1bc9665db37424c6863d1f98ad8f7fa32635279b
Shawn Rutledge <s@ecloud.org>
2016-12-08 08:12:59 +0100

    remove dead sunrise/sunset code
    since we're using that external stuff

--- android/app/src/main/java/org/ecloud/sologyr/WeatherService.java
+++ android/app/src/main/java/org/ecloud/sologyr/WeatherService.java
@@ -25,16 +25,6 @@ import java.util.TimeZone;
 
 import static android.location.LocationManager.PASSIVE_PROVIDER;
 
-/*
-import static java.lang.Math.acos;
-import static java.lang.Math.asin;
-import static java.lang.Math.atan;
-import static java.lang.Math.cos;
-import static java.lang.Math.floor;
-import static java.lang.Math.sin;
-import static java.lang.Math.tan;
-*/
-
 public class WeatherService extends Service {
 
     private List<WeatherListener> m_listeners = new ArrayList<>(2);
@@ -105,91 +95,6 @@ public class WeatherService extends Service {
         } else {
             Log.w(TAG, "requestRegularUpdates failed: provider " + m_periodicProvider);
         }
-    }
-
-    private double zeroToSomething(double limit, double val) {
-        double ret = val % limit;
-        if (ret < 0)
-            ret += limit;
-        return ret;
-    }
-
-    public void updateSunriseSunset(double latitude, double longitude) {
-        /*
-        // from http://williams.best.vwh.net/sunrise_sunset_algorithm.htm
-        Calendar cal = Calendar.getInstance();
-        int dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
-
-//        Sun's zenith for sunrise/sunset
-//            official     = 90 degrees 50'
-//            civil        = 96 degrees
-//            nautical     = 102 degrees
-//            astronomical = 108 degrees
-
-        double cosZenith = cos((90.0 + 50.0 / 60.0) * Math.PI / 180);
-        double cosLatitude = cos(latitude * Math.PI / 180);
-        double sinLatitude = sin(latitude * Math.PI / 180);
-        Log.d(TAG, "lat,lon " + latitude + ", " + longitude + " dayOfYear " + dayOfYear);
-
-        double lngHour = longitude / 15;
-
-        double rising = dayOfYear + ((6 - lngHour) / 24);
-        double setting = dayOfYear + ((18 - lngHour) / 24);
-        Log.d(TAG, "lngHour " + lngHour + " rising " + rising + " setting " + setting);
-
-        double meanAnomalyRising = (0.9856 * rising) - 3.289;
-        double meanAnomalySetting = (0.9856 * setting) - 3.289;
-
-        double sunLongitudeRising = zeroToSomething(360, meanAnomalyRising +
-                (1.916 * sin(meanAnomalyRising * Math.PI / 180)) +
-                (0.020 * sin(2 * meanAnomalyRising * Math.PI / 180)) + 282.634);
-        double sunLongitudeSetting = zeroToSomething(360, meanAnomalySetting +
-                (1.916 * sin(meanAnomalySetting * Math.PI / 180)) +
-                (0.020 * sin(2 * meanAnomalySetting * Math.PI / 180)) + 282.634);
-
-        Log.d(TAG, "sun longitudes " + sunLongitudeRising + ", " + sunLongitudeSetting);
-
-        double sunriseRA = atan(0.91764 * tan(sunLongitudeRising * Math.PI / 180)) * 180 / Math.PI;
-        double sunsetRA = atan(0.91764 * tan(sunLongitudeSetting * Math.PI / 180)) * 180 / Math.PI;
-
-        sunriseRA += (floor(sunLongitudeRising / 90) * 90 - floor(sunriseRA / 90) * 90) / 15; // in hours
-        sunsetRA += (floor(sunLongitudeSetting / 90) * 90 - floor(sunsetRA / 90) * 90) / 15;
-
-        Log.d(TAG, "sun RAs " + sunriseRA + ", " + sunsetRA);
-
-        double sunriseSinDeclination = 0.39782 * sin(sunLongitudeRising * Math.PI / 180);
-        double sunriseCosDeclination = cos(asin(sunriseSinDeclination));
-
-        double sunsetSinDeclination = 0.39782 * sin(sunLongitudeSetting * Math.PI / 180);
-        double sunsetCosDeclination = cos(asin(sunsetSinDeclination));
-
-        Log.d(TAG, "sun sin/cos DECs " + sunriseSinDeclination + ", " + sunriseCosDeclination + ", " + sunsetSinDeclination + ", " + sunsetCosDeclination);
-
-        double cosSunriseHourAngle = (cosZenith - (sunriseSinDeclination * sinLatitude)) /
-                (sunriseCosDeclination * cosLatitude);
-        double cosSunsetHourAngle = (cosZenith - (sunsetSinDeclination * sinLatitude)) /
-                (sunsetCosDeclination * cosLatitude);
-
-        Log.d(TAG, "sun hour angle cosines " + cosSunriseHourAngle + ", " + cosSunsetHourAngle);
-
-        // if (cosH >  1) the sun never rises on this location (on the specified date)
-        if (cosSunriseHourAngle > 1)
-            sunriseHour = 1;
-        else {
-            double sunriseHour = (360 - (acos(cosSunriseHourAngle) * 180 / Math.PI)) / 15 - 12;
-            double sunriseUTC = zeroToSomething(24, sunriseHour + sunriseRA - (0.06571 * rising) - 6.622 - lngHour);
-            Log.d(TAG, "sunrise " + sunriseHour + " UTC " + sunriseUTC);
-        }
-
-        // if (cosH < -1) the sun never sets on this location (on the specified date)
-        if (cosSunsetHourAngle < -1)
-            sunsetHour = -1;
-        else {
-            double sunsetHour = (acos(cosSunsetHourAngle) * 180 / Math.PI) / 15 + 12;
-            double sunsetUTC = zeroToSomething(24, sunsetHour + sunsetRA - (0.06571 * setting) - 6.622 - lngHour);
-            Log.d(TAG, "sunset " + sunsetHour + " UTC " + sunsetUTC);
-        }
-        */
     }
 
     public void setLocation(Location location) {
